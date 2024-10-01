bfa0a90fea82ab6ef6c417a4084e59d606aed3e4
Tan1213 <137980787+Tan1213@users.noreply.github.com>
2024-10-01 17:28:46 +0800

    api and db update

--- db.js
+++ db.js
@@ -2,6 +2,8 @@
 const db = require('express')();
 // Introduce custom API modules
 const dbApi = require('./api');
+// Introduce custom  defined path 
+const path = require('path');
 // Introduce body parser middleware for parsing request bodies
 const bodyParser = require('body-parser');
 // Using body parser middleware to parse request bodies in JSON and URL encoding formats
@@ -14,6 +16,15 @@ db.use(cors());
 // Mount the API module to the/mysql path
 db.use('/mysql', dbApi);
 // Start the server and listen on port 3200
+db.get("/Homepage",(req,res)=>{
+    res.sendFile(path.join(__dirname,'../view.html'));
+  });
+  db.get("/searchpage",(req,res)=>{
+    res.sendFile(path.join(__dirname,'../list.html'));
+  });
+  db.get("/fundraiserpage",(req,res)=>{
+    res.sendFile(path.join(__dirname,'../detail.html'));
+  });
 db.listen(3200);
 // Output prompt information
-console.log('Start Listening');
+console.log('Start in localhost:3200/Homepage');
